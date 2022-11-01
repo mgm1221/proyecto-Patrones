45954928855e0fa7b51d771784898ebf0219c520
nayenjoaquin <nayenjoaquin@gmail.com>
2022-11-01 20:15:45 -0300

    sadsad

--- main.cpp
+++ main.cpp
@@ -2,9 +2,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    string s = "tangananica_o_tanganana";
-    FMIndex fm(s);
-    cout << fm.count("tangana") << endl;
+int main(int argc, char const *argv[]) {
+
+    string textFileName = argv[1];
+    string pattern = argv[2];
+
+    ifstream textFile("data/" + textFileName);
+    string text((istreambuf_iterator<char>(textFile)), istreambuf_iterator<char>());
+
+    FMIndex fmIndex(text);
+    cout << fmIndex.count(pattern) << endl;
+
+
     return 0;
 }
