498bb476c99840473b8af83aa5302a9217136a0e
nayenjoaquin <nayenjoaquin@gmail.com>
2022-11-01 21:42:38 -0300

    kjhsda

--- main.cpp
+++ main.cpp
@@ -8,7 +8,14 @@ int main(int argc, char const *argv[]) {
     string pattern = argv[2];
 
     ifstream textFile("data/" + textFileName);
-    string text((istreambuf_iterator<char>(textFile)), istreambuf_iterator<char>());
+    string text;
+    string str;
+
+    int count = 0;
+    while(getline(textFile, str) && count <= 200) {
+        text += str;
+        count++;
+    };
 
     FMIndex fmIndex(text);
     cout << fmIndex.count(pattern) << endl;
